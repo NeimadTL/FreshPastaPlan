85334dff2494b5ed547a52e59412ab2003344698
neimad971 <damientalbot26@gmail.com>
2018-09-21 13:26:53 +0200

    the app has been configured for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_19_190926) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
